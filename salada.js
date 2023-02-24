23e08382c6230f77078447b6f00d2bc39380ec75
Leusiojosefilimone <leusiojosefilimone@gmail.com>
2023-02-24 16:23:32 -0300

    first commit
    esporta um vector que simula uma api de ingredientes

---
+++ salada.js
@@ -0,0 +1,15 @@
+const ingredientes = [  
+    {
+        nome: 'alface',
+        valor: 5
+    },
+    {
+        nome: 'cebola',
+        valor: 5
+    },
+    {
+        nome: 'tomate',
+        valor: 5
+    }
+ ];
+ export default ingredientes
