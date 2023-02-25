52736fd6f11fff1a76398e7fdce27a4cb76c41f2
Leusiojosefilimone <leusiojosefilimone@gmail.com>
2023-02-25 13:15:52 -0300

    actualiza fnCapitalize

--- modificador.js
+++ modificador.js
@@ -1,15 +1,33 @@
- export  function fnCapitalizar(vector){
-    var modificado = []
-
-    for (let i = 0; i < vector.length; i++) {
-        const letraInicial = vector[i].charAt(0).toUpperCase()
-        const restoTexto = vector[i].slice(1)
-        const resultado = letraInicial + restoTexto
-        modificado[i] = resultado
-        
+ export function fnCapitalizar(coletion, atrribute){
+    let result;
+    let error = null;
+        if(typeof coletion[0] == 'object'){
+                result = coletion.map(obj => {
+                const letraInicial = obj[atrribute].charAt(0).toUpperCase();
+                const restoTexto = obj[atrribute].slice(1)
+                obj[atrribute] = letraInicial + restoTexto
+            
+                return obj;
+        })
     }
-    return modificado;
-}
+    else if(typeof coletion[0] == 'string'){
+        result = coletion.map((obj, key) => {
+            const restoTexto = obj.slice(1)
+            const letraInicial = obj.charAt(0).toUpperCase()
+            obj = letraInicial + restoTexto
+
+           return obj
+        })
+       
+       
+    }
+    else{
+        error = 'valor invalido'
+    }
+   error ? console.log(error) : console.log(result)
+ }
+
+
 export function fnOrdenar (vector){
     return vector.sort(function(a, b){
         return a.localeCompare(b)
