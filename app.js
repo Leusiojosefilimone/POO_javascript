308f84d0e2e7da94e0a7394b408e9c8e980cb17e
Leusiojosefilimone <leusiojosefilimone@gmail.com>
2023-02-25 13:14:43 -0300

    adds tests lines
    that can be used to test fnCapitalize function

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 import ingredientes from "./salada.js";
-import {fnCapitalizar, fnOrdenar} from "./modificador.js";
+import { fnOrdenar, fnCapitalizar} from "./modificador.js";
 
-
+// var ingredientes2 = [1, 2 , 3, 4]
+// var ingredientes3 = ['tomate', 'molho', 'cebola', 'queijo']
+// fnCapitalizarNovo(ingredientes, 'nome')
+// fnCapitalizarNovo(ingredientes2, 'nome')
+// fnCapitalizarNovo(ingredientes3, 'nome')
    
 
 
